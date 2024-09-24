1414cb51519e19bf4ae7e822a5b4e68b062c2a68
Rajeev Sharma <rajeevsharmamachphy@gmail.com>
2024-09-24 07:20:07 +0530

    frontend update

---
+++ frontend/src/pages/Incidents.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Incidents = () => {
+    return (
+        <div>
+            <h2>Incidents</h2>
+            <p>List of monitored incidents will be displayed here.</p>
+        </div>
+    );
+};
+
+export default Incidents;
