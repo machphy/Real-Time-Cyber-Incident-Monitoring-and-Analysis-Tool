6f439dc16183a3254b6e089f5d240c46b1a4d85e
Rajeev Sharma <rajeevsharmamachphy@gmail.com>
2024-11-07 07:42:53 +0530

    newupdaean

---
+++ utils/logging_config.py
@@ -0,0 +1,13 @@
+import logging
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.FileHandler("logs/app.log"),
+        logging.StreamHandler()
+    ]
+)
+
+logger = logging.getLogger(__name__)
