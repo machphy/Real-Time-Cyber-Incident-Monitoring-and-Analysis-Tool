c24caf72db0e650cb1dd112792df8d3644d4b1ed
Rajeev Sharma <rajeevsharmamachphy@gmail.com>
2024-11-15 01:10:06 +0530

    database_test

---
+++ tests/test_api.py
@@ -0,0 +1,11 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_incident_route(client):
+    response = client.get('/api/incidents')
+    assert response.status_code == 200
