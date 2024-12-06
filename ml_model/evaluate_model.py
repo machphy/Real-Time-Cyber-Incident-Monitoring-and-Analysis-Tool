ca2921928f476ddb3c5c3fe4ebc151f1c604411b
rajeev <rajeevsharmamachphy@gmail.com>
2024-12-06 15:46:24 +0530

    update

--- ml_model/evaluate_model.py
+++ ml_model/evaluate_model.py
@@ -14,6 +14,6 @@ y_pred = model.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 report = classification_report(y_test, y_pred)
 
-print(f'Accuracy: {accuracy}')      #for accuracy ok
+print(f'Accuracy: {accuracy}')  
 print('Classification Report here:')
 print(report)
